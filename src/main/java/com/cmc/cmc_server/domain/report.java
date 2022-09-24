04249df0d89278f6faeb59d4b18edfb1f3acd0e5
isExample <yeji0331@naver.com>
2022-09-24 21:12:30 +0900

    [Feat] 레포트 엔티티 추가

---
+++ src/main/java/com/cmc/cmc_server/domain/report.java
@@ -0,0 +1,33 @@
+package com.cmc.cmc_server.domain;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = PROTECTED)
+@ApiModel("신고 정보")
+public class report {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @ApiModelProperty(value = "신고 idx", example = "1")
+    private Long reportId;
+
+    @ApiModelProperty(value = "사용자 idx", example = "1")
+    private Long userId;
+
+    @ApiModelProperty(value = "스토리 idx", example = "1")
+    private Long storyId;
+}
