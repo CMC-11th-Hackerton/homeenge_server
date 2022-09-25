0bd7fc5dcfacd189c3720323292b34340fb64b4e
minu kim <kmw106933@naver.com>
2022-09-25 11:27:29 +0900

    [Refactor] 챌린지 API 리팩토링

--- src/main/java/com/cmc/cmc_server/application/ImageService.java
+++ src/main/java/com/cmc/cmc_server/application/ImageService.java
@@ -1,48 +1,48 @@
-package com.cmc.cmc_server.application;
-
-import com.cmc.cmc_server.domain.PostImage;
-import com.cmc.cmc_server.dto.Image.ImageReq;
-import com.cmc.cmc_server.dto.Image.ImageRes;
-import com.cmc.cmc_server.infra.PostImageRepository;
-import com.cmc.cmc_server.s3.S3Uploader;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-@RequiredArgsConstructor
-@Transactional
-public class ImageService {
-
-//    private final S3Uploader s3Uploader;
-//    private final PostImageRepository postImageRepository;
+//package com.cmc.cmc_server.application;
 //
-//    public ImageRes createPost(ImageReq imageReq) {
-//        List<String> postImages = uploadPostImages(imageReq);
-//        return ImageRes.builder().imageUrl(postImages).build();
-//    }
+//import com.cmc.cmc_server.domain.PostImage;
+//import com.cmc.cmc_server.dto.Image.ImageReq;
+//import com.cmc.cmc_server.dto.Image.ImageRes;
+//import com.cmc.cmc_server.infra.PostImageRepository;
+//import com.cmc.cmc_server.s3.S3Uploader;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
 //
-//    /**
-//     * 이미지 파일 S3 저장 + PostImage 생성
-//     */
-//    private List<String> uploadPostImages(ImageReq imageReq) {
-////        return imageReq.getImageFiles().stream()
-////                .map(image -> s3Uploader.upload(image, "post"))
-////                .map(url -> createPostImage(url))
-////                .map(postImage -> postImage.getImageUrl())
-////                .collect(Collectors.toList());
-//    }
+//import java.util.List;
+//import java.util.stream.Collectors;
 //
-//    /**
-//     * PostImage 생성 메서드
-//     */
-//    private PostImage createPostImage(String url) {
-//        System.out.println("url = " + url);
-//        return postImageRepository.save(PostImage.builder()
-//                .imageUrl(url)
-//                .build());
-//    }
-}
+//@Service
+//@RequiredArgsConstructor
+//@Transactional
+//public class ImageService {
+//
+////    private final S3Uploader s3Uploader;
+////    private final PostImageRepository postImageRepository;
+////
+////    public ImageRes createPost(ImageReq imageReq) {
+////        List<String> postImages = uploadPostImages(imageReq);
+////        return ImageRes.builder().imageUrl(postImages).build();
+////    }
+////
+////    /**
+////     * 이미지 파일 S3 저장 + PostImage 생성
+////     */
+////    private List<String> uploadPostImages(ImageReq imageReq) {
+//////        return imageReq.getImageFiles().stream()
+//////                .map(image -> s3Uploader.upload(image, "post"))
+//////                .map(url -> createPostImage(url))
+//////                .map(postImage -> postImage.getImageUrl())
+//////                .collect(Collectors.toList());
+////    }
+////
+////    /**
+////     * PostImage 생성 메서드
+////     */
+////    private PostImage createPostImage(String url) {
+////        System.out.println("url = " + url);
+////        return postImageRepository.save(PostImage.builder()
+////                .imageUrl(url)
+////                .build());
+////    }
+//}
--- src/main/java/com/cmc/cmc_server/controllers/ImageController.java
+++ src/main/java/com/cmc/cmc_server/controllers/ImageController.java
@@ -1,28 +1,28 @@
-package com.cmc.cmc_server.controllers;
-
-import com.cmc.cmc_server.application.ImageService;
-import com.cmc.cmc_server.dto.Image.ImageReq;
-import com.cmc.cmc_server.dto.Image.ImageRes;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/images")
-@RequiredArgsConstructor
-@Api(tags = "이미지 API")
-public class ImageController {
-
-    private final ImageService imageService;
-
-    @ApiOperation(value = "이미지 생성", notes = "Form Data 값을 받아와서 이미지를 생성하는 API",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @PostMapping(consumes = {"multipart/form-data"})
-    public ImageRes createPost(@ModelAttribute ImageReq imageReq){
-
-        return imageService.createPost(imageReq);
-    }
-
-}
+//package com.cmc.cmc_server.controllers;
+//
+//import com.cmc.cmc_server.application.ImageService;
+//import com.cmc.cmc_server.dto.Image.ImageReq;
+//import com.cmc.cmc_server.dto.Image.ImageRes;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.bind.annotation.*;
+//
+//@RestController
+//@RequestMapping("/images")
+//@RequiredArgsConstructor
+//@Api(tags = "이미지 API")
+//public class ImageController {
+//
+////    private final ImageService imageService;
+////
+////    @ApiOperation(value = "이미지 생성", notes = "Form Data 값을 받아와서 이미지를 생성하는 API",
+////            consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+////    @PostMapping(consumes = {"multipart/form-data"})
+////    public ImageRes createPost(@ModelAttribute ImageReq imageReq){
+////
+////        return imageService.createPost(imageReq);
+////    }
+//
+//}
